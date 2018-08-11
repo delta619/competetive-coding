3160285445dfc3ccef09fb14274ed06d6d2e4f8a
Ashutosh Malla <deltareaper619@gmail.com>
2018-08-11 14:26:00 +0530

    Initial commit

---
+++ Anagram.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+char a[7000000], b[7000000];
+long int l1, l2;
+
+int main()
+{
+	cin.getline(a, 7000000);
+	cin.getline(b, 7000000);
+
+		l1 = strlen(a);
+		l2 = strlen(b);
+		if (l1 != l2)
+		{
+			cout << "NO";
+		}
+		else
+		{
+			
+				sort(a, a+l1);	
+				sort(b, b+l2);
+				
+			int flag = 1;
+
+			for (long int i = 0; i < l1; i++)
+			{
+				if (a[i] != b[i])
+				{
+					flag = 0;
+				}
+			}
+			flag==0 ? cout << "NO" : cout << "YES";
+
+		}
+	
+
+//	system("pause");
+	return 0;
+
+}
+	
+	
+
