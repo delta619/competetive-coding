f5618d7052f8525ff7718a42c6923281e9fe2382
ashu <ashu@ashus-MacBook-Pro.local>
2024-06-06 04:38:44 -0700

    2d point coint

--- dp/stock IV.go
+++ dp/stock IV.go
@@ -15,31 +15,31 @@ func maxProfit(k int, prices []int) int {
     if k >= n/2 {
         return maxProfitInf(prices)
     }
-
+    // dp array to store the maximum profit for each day and transaction count.
     dp := make([][][]int, n)
-    for i:=0 ;i< n; i++{
+    // Initializing the dp array.
+    for i := 0; i < n; i++ {
         dp[i] = make([][]int, k+1)
-        for j:=0 ;j<=k ;j++{
+        for j := 0; j <= k; j++ {
             dp[i][j] = make([]int, 2)
         }
     }
-
-    for i:=0 ;i< n;i++{
-        for j:=0; j<=k ;j++{
-            if i == 0{
+    // Dynamic programming to calculate maximum profit.
+    for i := 0; i < n; i++ {
+        for j := 1; j <=k; j++ {
+            if i == 0 {
+                // Base case: no previous day, initialize profit values.
                 dp[i][j][0] = 0
                 dp[i][j][1] = -prices[i]
                 continue
             }
-
-            dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i])
-            dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j][0] - prices[i])
+            // Calculate the maximum profit for each day and transaction count.
+            dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+            dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
         }
     }
-
-
+    // Return the maximum profit achievable.
     return dp[n-1][k][0]
-
 }
 
 // Function maxProfitInf calculates the maximum profit achievable with unlimited transactions.
