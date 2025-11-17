1f6e72e4da42b074626fafa753cbbf05faa4a79f
= <=>
2025-11-17 14:17:42 +0530

    added another code for contains_duplicate

--- array/traverse/contains_duplicate.js
+++ array/traverse/contains_duplicate.js
@@ -13,3 +13,20 @@ var containsDuplicate = function(nums) {
     }
     return false;
 };
+
+/**
+ * Checks whether the given array contains any duplicate elements, using Set.
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let m = new Set()
+
+    for (let i = 0 ;i < nums.length ;i++){
+        if (m.has(nums[i])){ // syntactical sugar for hasOwnProperty
+            return true
+        }
+        m.add(nums[i], true) 
+    }
+    return false;
+};
