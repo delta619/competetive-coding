688280cab850ec4bd44337b9ed577750d732e0c3
= <deltareaper619@gmail.com>
2025-11-29 13:48:29 +0530

    container with most water.js

---
+++ array/greedy/container with most water.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let l = 0
+    let r = height.length - 1
+    let area = 0;
+    while (l < r) {
+
+        let temp_area = (r - l) * Math.min(height[l], height[r])
+        if (temp_area > area) {
+            area = temp_area;
+        }
+
+        if (height[l] === height[r]) {
+            l++
+            r--;
+        }
+        else if (height[l] < height[r]) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+    return area
+};
