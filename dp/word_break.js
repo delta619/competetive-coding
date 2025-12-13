3c3e2f400934504f467c2e0776b2ab7400341a26
= <deltareaper619@gmail.com>
2025-12-13 21:36:53 +0530

    added wordbreak.js

---
+++ dp/word_break.js
@@ -0,0 +1,26 @@
+/**
+ * Leetcode 139. Word Break
+ * https://leetcode.com/problems/word-break/
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+    const wordSet = new Set(wordDict);
+    const n = s.length;
+
+    const dp = new Array(n + 1).fill(false);
+    dp[0] = true;
+    
+    for(let i = 1 ; i<= s.length; i++){
+        for(let j = 0 ; j< i ; j++){
+            if(dp[j] == true && wordSet.has(s.substring(j, i))){
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+
+    return dp[n];
+};
