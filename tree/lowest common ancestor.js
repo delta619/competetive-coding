df1d1245553c43c3e7d38bee5852b227bde359a5
= <deltareaper619@gmail.com>
2025-12-19 10:19:14 +0530

    added tree problems js

---
+++ tree/lowest common ancestor.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Leetcode 235. Lowest Common Ancestor of a Binary Search Tree
+ * 
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+
+        if(root === null) return null;
+
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left, p, q)
+        }
+
+        if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right, p, q)
+        }
+
+        return root
+
+};
