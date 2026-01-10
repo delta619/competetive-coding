e602f268e5bff785a10d8eccda079bd67d0a2fae
= <deltareaper619@gmail.com>
2026-01-10 21:44:32 +0530

    word bread py

---
+++ dp/word break.py
@@ -0,0 +1,16 @@
+# 139. Word Break
+
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False for _ in range(len(s)+1)]
+        dp[0] = True
+        for i in range (1, len(s)+1):
+            for j in range(i):
+                if dp[j] == True and s[j:i] in wordDict:
+                    dp[i] = True
+        
+        return dp[len(s)]
+        
